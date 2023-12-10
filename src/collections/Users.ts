f5fa276eeb50a9081f0e4b6897a3d7ac10f8be7d
Duy Nguyen <buiduynguyen@Nguyens-MacBook-Pro.local>
2023-12-10 11:17:30 +0700

    Finish Modeling and Preview Page

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,5 +1,14 @@
-import { verify } from 'crypto';
-import { CollectionConfig } from 'payload/types';
+import { Access, CollectionConfig } from 'payload/types';
+
+const adminsAndUser: Access = ({ req: { user } }) => {
+  if (user.role === 'admin') return true;
+
+  return {
+    id: {
+      equals: user.id,
+    },
+  };
+};
 
 export const Users: CollectionConfig = {
   slug: 'users',
@@ -11,17 +20,40 @@ export const Users: CollectionConfig = {
     },
   },
   access: {
-    read: () => true,
+    read: adminsAndUser,
     create: () => true,
+    update: ({ req }) => req.user.role === 'admin',
+    delete: ({ req }) => req.user.role === 'admin',
+  },
+  admin: {
+    hidden: ({ user }) => user.role !== 'admin',
+    defaultColumns: ['id'],
   },
   fields: [
+    {
+      name: 'products',
+      label: 'Products',
+      admin: {
+        condition: () => false,
+      },
+      type: 'relationship',
+      relationTo: 'products',
+      hasMany: true,
+    },
+    {
+      name: 'product_files',
+      label: 'Product files',
+      admin: {
+        condition: () => false,
+      },
+      type: 'relationship',
+      relationTo: 'product_files',
+      hasMany: true,
+    },
     {
       name: 'role',
       required: true,
       defaultValue: 'user',
-      admin: {
-        condition: () => false,
-      },
       type: 'select',
       options: [
         { label: 'Admin', value: 'admin' },
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,13 @@ import { Icons } from './Icons';
 import { NavItems } from './NavItems';
 import { buttonVariants } from './ui/button';
 import { Cart } from './Cart';
+import { getServerSideUser } from '@/lib/payload-utils';
+import { cookies } from 'next/headers';
+import { UserAccountNav } from './UserAccountNav';
 
-export const Navbar = () => {
-  const user = null;
+export const Navbar = async () => {
+  const nextCookies = cookies();
+  const { user } = await getServerSideUser(nextCookies);
 
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
@@ -37,7 +41,7 @@ export const Navbar = () => {
                     <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
                   )}
                   {user ? (
-                    <></>
+                    <UserAccountNav user={user} />
                   ) : (
                     <Link
                       href="/sign-up"
